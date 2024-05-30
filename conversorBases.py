a517de74cf67dade671cb8d53543bedbce1c4b71
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2024-05-30 11:55:52 -0300

    3.0 conversorBases.py

--- conversorBases.py
+++ conversorBases.py
@@ -31,11 +31,79 @@ def converter_para_hexadecimal():
         resultado_label.configure(text="Por favor, insira um número válido.")
 
 
+def converter_para_decimal():
+    try:
+        num_binario = num_entry_2.get()
+        num_decimal = int(num_binario, 2)
+        resultado_label.configure(text=f"Número inserido: {
+                                  num_binario}\nResultado: {num_decimal}")
+    except ValueError:
+        resultado_label.configure(
+            text="Por favor, insira um número binário válido.")
+
+
+def converter_de_binario_para_octal():
+    try:
+        num_binario = num_entry_2.get()
+        num_decimal = int(num_binario, 2)
+        resultado = oct(num_decimal)[2:]
+        resultado_label.configure(text=f"Número inserido: {
+                                  num_binario}\nResultado: {resultado}")
+    except ValueError:
+        resultado_label.configure(
+            text="Por favor, insira um número binário válido.")
+
+
+def converter_de_binario_para_hexadecimal():
+    try:
+        num_binario = num_entry_2.get()
+        num_decimal = int(num_binario, 2)
+        resultado = hex(num_decimal)[2:]
+        resultado_label.configure(text=f"Número inserido: {
+                                  num_binario}\nResultado: {resultado}")
+    except ValueError:
+        resultado_label.configure(
+            text="Por favor, insira um número binário válido.")
+
+
+def limparJanela():
+    for widget in janela.winfo_children():
+        widget.destroy()
+
+    texto_pagina_2 = customtkinter.CTkLabel(
+        janela, text="Conversor de Bases Inversa", font=("Arial", 20))
+    texto_pagina_2.pack(padx=10, pady=10)
+
+    global num_entry_2
+    num_entry_2 = customtkinter.CTkEntry(
+        janela, placeholder_text="Número Binário")
+    num_entry_2.pack(padx=10, pady=10)
+
+    botao_binario_decimal = customtkinter.CTkButton(
+        janela, text="Converter para Decimal", command=converter_para_decimal)
+    botao_binario_decimal.pack(padx=10, pady=10)
+
+    botao_binario_octal = customtkinter.CTkButton(
+        janela, text="Converter para Octal", command=converter_de_binario_para_octal)
+    botao_binario_octal.pack(padx=10, pady=10)
+
+    botao_binario_hexadecimal = customtkinter.CTkButton(
+        janela, text="Converter para Hexadecimal", command=converter_de_binario_para_hexadecimal)
+    botao_binario_hexadecimal.pack(padx=10, pady=10)
+
+    global resultado_label
+    resultado_label = customtkinter.CTkLabel(janela, text="Resultado: ")
+    resultado_label.pack(padx=10, pady=10)
+
+    autor_label = customtkinter.CTkLabel(janela, text="Autor: Gabriel Cézar")
+    autor_label.place(relx=1.0, rely=1.0, anchor='se', x=-10, y=-10)
+
+
 # criação da janela
 janela = customtkinter.CTk()
 janela.geometry("550x550")
 janela.maxsize(width=850, height=850)
-janela.maxsize(width=550, height=550)
+janela.minsize(width=550, height=550)
 
 # adiciona uma label
 texto = customtkinter.CTkLabel(
@@ -61,7 +129,7 @@ botao3.pack(padx=10, pady=10)
 
 # adiciona um botão para a conversão contrária [WIP]
 botao_conversao_contraria = customtkinter.CTkButton(
-    janela, text="Converter de Binário/Octal/Hexadecimal para Decimal")
+    janela, text="Converter de BINÁRIO para Octal/Hexadecimal/Decimal", command=limparJanela)
 botao_conversao_contraria.place(relx=1.0, rely=1.0, anchor='se', x=-10, y=-50)
 
 # adiciona uma label para mostrar o resultado
